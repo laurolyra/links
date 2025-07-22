f2f300df09032eb8f6090876d5bdc1834a4010b9
Lauro Lyra <laurolyra@gmail.com>
2025-07-22 11:53:10 -0300

    fix app height

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { PageContainer } from './components/PageContainer';
 
 function App() {
   return (
-    <div className="bg-blue-400 h-screen flex items-center justify-center @container">
+    <div className="bg-blue-400 h-screen flex items-center justify-center @container overflow-hidden">
       <PageContainer />
     </div>
   );
