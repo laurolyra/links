b213e85d9cacd296105e689507210f8b22e96cc4
Lauro Lyra <laurolyra@gmail.com>
2025-07-22 09:38:46 -0300

    changing container for responsive queries

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,10 +1,11 @@
 interface ILinkButton {
   url: string;
   label: string;
+  style: string;
 }
-export const LinkButton = ({ url, label }: ILinkButton) => {
+export const LinkButton = ({ url, label, style }: ILinkButton) => {
   return (
-    <a href={url} target="_blank" rel="noreferrer">
+    <a href={url} target="_blank" rel="noreferrer" className={style}>
       {label}
     </a>
   );
--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -3,21 +3,21 @@ import { LinkButton } from './LinkButton';
 
 export const PageContainer = () => {
   return (
-    <div className="w-10/12 h-11/12 p-2 @md:max-w-3/12 @md:h-fit @md:p-4 @md:py-6 bg-white rounded-lg shadow-[0px_0px_19px_17px_rgba(0,_0,_0,_0.2)]">
+    <div className="max-w-10/12 p-6 @md:max-w-8/12 bg-white rounded-lg shadow-[0px_0px_19px_17px_rgba(0,_0,_0,_0.2)]">
       <div className="pb-4">
-        <h1 className="text-2xl @md:text-4xl text-center">Lauro Lyra Aguiar</h1>
-        <p className="text-xs @md:text-base text-center">
+        <h1 className="text-2xl @lg:text-4xl text-center">Lauro Lyra Aguiar</h1>
+        <p className="text-xs @lg:text-base text-center">
           Full Stack Software Developer
         </p>
       </div>
       <div className="flex flex-col gap-2 items-center justify-center w-10/12 mx-auto">
         {LINKS.map(({ label, url }) => (
-          <div
+          <LinkButton
             key={url}
-            className="bg-green-400 p-2 px-4 rounded-lg text-2xl @md:text-l text-center w-full"
-          >
-            <LinkButton url={url} label={label} />
-          </div>
+            url={url}
+            label={label}
+            style="bg-green-400 p-2 px-4 rounded-lg text-l @lg:text-2xl text-center w-full"
+          />
         ))}
       </div>
     </div>
